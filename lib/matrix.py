4ace04ee4b9111cd37a4f5247e71e757f3c49c9e
Gökberk AKDENİZ <gokberkakdeniz@hotmail.com>
2021-10-25 00:09:22 +0300

    mat3d implemented

--- lib/matrix.py
+++ lib/matrix.py
@@ -3,5 +3,144 @@
 # StudentId:250201041
 # 10 2021
 
-class mat3d:
-    pass
+from typing import Union
+from math import sin, cos
+from . import ensure
+from .vector import Vec3d
+
+
+class Mat3d:
+    """
+    ```
+    Mat3d = [Vec3d([x1 y1 z1 w1])
+             Vec3d([x2 y2 z2 w2])
+             Vec3d([x3 y3 z3 w3])
+             Vec3d([x4 y4 z4 w4])]
+    ```
+    """
+
+    def __init__(self, r1: Vec3d, r2: Vec3d, r3: Vec3d, r4: Vec3d) -> None:
+        ensure.type_of(r1, "r1", [Vec3d])
+        ensure.type_of(r2, "r2", [Vec3d])
+        ensure.type_of(r3, "r3", [Vec3d])
+        ensure.type_of(r4, "r4", [Vec3d])
+
+        self.matrix = [r1, r2, r3, r4]
+
+    @staticmethod
+    def translation_matrix(tx: float, ty: float, tz: float):
+        return Mat3d(Vec3d(1, 0, 0, tx),
+                     Vec3d(0, 1, 0, ty),
+                     Vec3d(0, 0, 1, tz),
+                     Vec3d(0, 0, 0, 1))
+
+    @staticmethod
+    def scaling_matrix(sx: float, sy: float, sz: float):
+        return Mat3d(Vec3d(sx, 0, 0, 0),
+                     Vec3d(0, sy, 0, 0),
+                     Vec3d(0, 0, sz, 0),
+                     Vec3d(0, 0, 0, 1),)
+
+    @staticmethod
+    def rotation_x_matrix(theta: float):
+        return Mat3d(Vec3d(1, 0, 0, 0),
+                     Vec3d(0, cos(theta), -sin(theta), 0),
+                     Vec3d(0, sin(theta), cos(theta), 0),
+                     Vec3d(0, 0, 0, 1))
+
+    @staticmethod
+    def rotation_y_matrix(theta: float):
+        return Mat3d(Vec3d(cos(theta), 0, sin(theta), 0),
+                     Vec3d(0, 1, 0, 0),
+                     Vec3d(-sin(theta), 0, cos(theta), 0),
+                     Vec3d(0, 0, 0, 1))
+
+    @staticmethod
+    def rotation_z_matrix(theta: float):
+        return Mat3d(Vec3d(cos(theta), -sin(theta), 0, 0),
+                     Vec3d(sin(theta), cos(theta), 0, 0),
+                     Vec3d(0, 0, 1, 0),
+                     Vec3d(0, 0, 0, 1))
+
+    def clone(self):
+        return Mat3d(self.matrix[0].clone(),
+                     self.matrix[1].clone(),
+                     self.matrix[2].clone(),
+                     self.matrix[3].clone())
+
+    def __add__(self, mat2: Union[int, float]):
+        ensure.type_of(mat2, "operand", [Mat3d])
+
+        return Mat3d(self.matrix[0] + mat2.matrix[0],
+                     self.matrix[1] + mat2.matrix[1],
+                     self.matrix[2] + mat2.matrix[2],
+                     self.matrix[3] + mat2.matrix[3])
+
+    def __radd__(self, mat2: Union[int, float]):
+        return self + mat2
+
+    def __sub__(self, mat2: Union[int, float]):
+        ensure.type_of(mat2, "operand", [Mat3d])
+
+        return self + (-mat2)
+
+    def __rsub__(self, mat2: Union[int, float]):
+        return self - mat2
+
+    def __mul__(self, scalar: Union[int, float]):
+        ensure.number(scalar, "operand")
+
+        return Mat3d(self.matrix[0] * scalar,
+                     self.matrix[1] * scalar,
+                     self.matrix[2] * scalar,
+                     self.matrix[3] * scalar)
+
+    def __rmul__(self, scalar: Union[int, float]):
+        return self * scalar
+
+    def __matmul__(self, mat):
+        ensure.type_of(mat, "mat", [Vec3d, Mat3d])
+
+        if type(mat) is Vec3d:
+            return Vec3d(self.matrix[0].dot(mat),
+                         self.matrix[1].dot(mat),
+                         self.matrix[2].dot(mat),
+                         self.matrix[3].dot(mat))
+
+        mat_T = mat.transpose()
+
+        return Mat3d(*[Vec3d(*[row.dot(row_T) for row_T in mat_T]) for row in self.matrix])
+
+    def transpose(self):
+        return Mat3d(
+            Vec3d(self.matrix[0].x, self.matrix[1].x,
+                  self.matrix[2].x, self.matrix[3].x),
+            Vec3d(self.matrix[0].y, self.matrix[1].y,
+                  self.matrix[2].y, self.matrix[3].y),
+            Vec3d(self.matrix[0].z, self.matrix[1].z,
+                  self.matrix[2].z, self.matrix[3].z),
+            Vec3d(self.matrix[0].w, self.matrix[1].w,
+                  self.matrix[2].w, self.matrix[3].w)
+        )
+
+    def __eq__(self, o: object) -> bool:
+        return type(o) and type(self) and \
+            self.matrix[0] == o.matrix[0] and \
+            self.matrix[1] == o.matrix[1] and \
+            self.matrix[2] == o.matrix[2] and \
+            self.matrix[3] == o.matrix[3]
+
+    def __getitem__(self, index):
+        return self.matrix[index]
+
+    def __setitem__(self, index, value):
+        self.matrix[index] = value
+
+    def __neg__(self):
+        return -1 * self
+
+    def __pos__(self):
+        return self.clone()
+
+    def __str__(self) -> str:
+        return "mat3d(" + ",\n      ".join(map(str, self.matrix)) + ")"
---
+++ tests/test_matrix.py
@@ -0,0 +1,124 @@
+# CENG 487 Assignment1 by
+# Gokberk Akdeniz
+# StudentId:250201041
+# 10 2021
+
+import unittest
+from math import pi
+from lib.matrix import Mat3d
+from lib.vector import Vec3d
+
+
+class vec3dTest(unittest.TestCase):
+    def setUp(self):
+        self.zero = Mat3d(
+            Vec3d(0, 0, 0, 0),
+            Vec3d(0, 0, 0, 0),
+            Vec3d(0, 0, 0, 0),
+            Vec3d(0, 0, 0, 0),
+        )
+        self.identity = Mat3d(
+            Vec3d(1, 0, 0, 0),
+            Vec3d(0, 1, 0, 0),
+            Vec3d(0, 0, 1, 0),
+            Vec3d(0, 0, 0, 1),
+        )
+        self.matrix = Mat3d(
+            Vec3d(1, 0, 0, 2),
+            Vec3d(0, 1, 0, 3),
+            Vec3d(0, 0, 1, 4),
+            Vec3d(0, 0, 0, 1),
+        )
+        self.point = Vec3d.point(1, 2, 3)
+
+    def test_equality(self):
+        self.assertEqual(self.identity, self.identity)
+        self.assertEqual(
+            Mat3d(
+                Vec3d(1, 0, 444, 2),
+                Vec3d(-55, 1, 0.55, 3),
+                Vec3d(0, 9, 1, 4),
+                Vec3d(111, pi, 1/3, 1),
+            ), Mat3d(
+                Vec3d(1, 0, 444, 2),
+                Vec3d(-55, 1, 0.55, 3),
+                Vec3d(0, 9, 1, 4),
+                Vec3d(111, pi, 1/3, 1),
+            )
+        )
+
+    def test_clone(self):
+        self.assertEqual(self.identity, self.identity.clone())
+
+    def test_unary_minus(self):
+        self.assertEqual(-(-self.identity), self.identity)
+
+    def test_unary_plus(self):
+        self.assertEqual(+self.identity, self.identity)
+
+    def test_matrix_multiply_scalar(self):
+        self.assertEqual(self.zero * 5, self.zero)
+        self.assertEqual(0 * self.identity, self.zero)
+        self.assertEqual(pi * self.zero, self.zero)
+        self.assertEqual(
+            3 * self.identity,
+            Mat3d(
+                Vec3d(3, 0, 0, 0),
+                Vec3d(0, 3, 0, 0),
+                Vec3d(0, 0, 3, 0),
+                Vec3d(0, 0, 0, 3),
+            )
+        )
+
+    def test_matrix_plus_matrix(self):
+        self.assertEqual(self.zero + self.zero, self.zero)
+        self.assertEqual(self.zero + self.matrix, self.matrix)
+
+    def test_matrix_minus_matrix(self):
+        self.assertEqual(self.zero - self.zero, self.zero)
+        self.assertEqual(self.zero - self.matrix, -self.matrix)
+
+    def test_matrix_multiply_matrix(self):
+        self.assertEqual(self.identity @ self.matrix, self.matrix)
+        self.assertEqual(self.matrix @ self.identity, self.matrix)
+        self.assertEqual(self.matrix @ self.zero, self.zero)
+        self.assertEqual(
+            self.matrix @ self.identity @ self.identity,
+            self.matrix
+        )
+        self.assertEqual(
+            Mat3d(
+                Vec3d(1, 2, 3, 4),
+                Vec3d(5, 6, 7, 8),
+                Vec3d(9, 10, 11, 12),
+                Vec3d(13, 14, 15, 16),
+            ) @ Mat3d(
+                Vec3d(17, 18, 19, 20),
+                Vec3d(21, 22, 23, 24),
+                Vec3d(25, 26, 27, 28),
+                Vec3d(29, 30, 31, 32),
+            ),
+            Mat3d(
+                Vec3d(250, 260, 270, 280),
+                Vec3d(618, 644, 670, 696),
+                Vec3d(986, 1028, 1070, 1112),
+                Vec3d(1354, 1412, 1470, 1528),
+            )
+        )
+
+    def test_matrix_multiply_vector(self):
+        self.assertEqual(
+            self.identity @ self.point, self.point
+        )
+        self.assertEqual(
+            Mat3d(
+                Vec3d(1, 0, 0, 10),
+                Vec3d(0, 1, 0, 11),
+                Vec3d(0, 0, 1, 12),
+                Vec3d(0, 0, 0, 1),
+            ) @ self.point,
+            self.point + Vec3d(10, 11, 12, 0)
+        )
+
+if __name__ == '__main__':
+    unittest.main()
