23f57967a0fdbe06899d647fd563f23237977578
Gökberk AKDENİZ <gokberkakdeniz@hotmail.com>
2021-10-25 00:10:31 +0300

    involve w in vector operations

--- lib/vector.py
+++ lib/vector.py
@@ -8,63 +8,80 @@ from typing import Union
 from . import ensure
 
 
-class vec3d:
+class Vec3d:
+    """
+    ```
+    Vec3d = [x
+             y
+             z
+             w]
+    ```
+    """
+
     def __init__(self, x: float, y: float, z: float, w: int):
         ensure.number(x, "x")
         ensure.number(y, "y")
         ensure.number(y, "z")
-        ensure.one_of(w, "w", [0, 1])
+        ensure.number(w, "w")
 
         self.x = float(x)
         self.y = float(y)
         self.z = float(z)
-        self.__w = w
+        self.w = float(w)
+        self.order = ["x", "y", "z", "w"]
 
-    @classmethod
-    def point(cls, x, y, z):
-        return cls(x, y, z, 1)
+    @staticmethod
+    def point(x, y, z):
+        return Vec3d(x, y, z, 1)
 
-    @classmethod
-    def vector(cls, x, y, z):
-        return cls(x, y, z, 0)
+    @staticmethod
+    def vector(x, y, z):
+        return Vec3d(x, y, z, 0)
 
     def clone(self):
-        return vec3d(self.x, self.y, self.z, self.__w)
+        return Vec3d(self.x, self.y, self.z, self.w)
 
     def to_point(self):
-        return vec3d(self.x, self.y, self.z, 1)
+        return Vec3d(self.x, self.y, self.z, 1)
 
     def to_vector(self):
-        return vec3d(self.x, self.y, self.z, 0)
+        return Vec3d(self.x, self.y, self.z, 0)
 
     def __str__(self) -> str:
-        return "{}({}, {}, {})".format("P" if self.__w else "V", self.x, self.y, self.z)
+        return "vec3d({}, {}, {}, {})".format(self.x, self.y, self.z, self.w)
 
     def is_point(self):
-        return self.__w == 1
+        return self.w == 1
 
     def is_vector(self):
-        return self.__w == 0
+        return self.w == 0
 
     def cast_to_point(self):
-        self.__w = 1
+        self.w = 1
 
     def cast_to_vector(self):
-        self.__w = 0
+        self.w = 0
 
     def dot(self, o):
-        ensure.type_of(o, "operand", [vec3d])
+        ensure.type_of(o, "operand", [Vec3d])
 
-        return self.x * o.x + self.y * o.y + self.z * o.z + self.__w * o.__w
+        return self.x * o.x + self.y * o.y + self.z * o.z + self.w * o.w
 
     def cross(self, vec2):
-        return None
+        ensure.type_of(vec2, "operand", [Vec3d])
+        if self.w != vec2.w != 0:
+            raise TypeError("expected vector not point")
+
+        return Vec3d(self.y * vec2.z - self.z * vec2.y,
+                     self.z * vec2.x - self.x * vec2.z,
+                     self.x * vec2.y - self.y * vec2.x,
+                     0)
 
     def project(self, vec2):
         return None
 
     def angle(self, o, degree=False):
-        ensure.type_of(o, "operand", [vec3d])
+        ensure.type_of(o, "operand", [Vec3d])
 
         radian = acos(self.dot(o) / (self.length() * o.length()))
 
@@ -77,21 +94,15 @@ class vec3d:
         return self / self.length()
 
     def __add__(self, o: object):
-        ensure.type_of(o, "operand", [vec3d])
+        ensure.type_of(o, "operand", [Vec3d])
 
-        if self.__w != o.__w:
-            raise TypeError("'w's are not same.")
-
-        return vec3d(o.x + self.x, o.y + self.y, o.z + self.z, self.__w)
+        return Vec3d(o.x + self.x, o.y + self.y, o.z + self.z, o.w + self.w)
 
     def __radd__(self, o: object):
         return o + self
 
     def __sub__(self, o: object):
-        ensure.type_of(o, "operand", [vec3d])
-
-        if self.__w != o.__w:
-            raise TypeError("'w's are not same.")
+        ensure.type_of(o, "operand", [Vec3d])
 
         return self + (-o)
 
@@ -107,20 +118,26 @@ class vec3d:
     def __mul__(self, scalar: Union[int, float]):
         ensure.number(scalar, "operand")
 
-        return vec3d(scalar * self.x, scalar * self.y, scalar * self.z, self.__w)
+        return Vec3d(scalar * self.x, scalar * self.y, scalar * self.z, scalar * self.w)
 
     def __rmul__(self, scalar: Union[int, float]):
         return self * scalar
 
     def __truediv__(self, scalar):
-        return vec3d(self.x / scalar, self.y / scalar, self.z / scalar, self.__w)
+        return Vec3d(self.x / scalar, self.y / scalar, self.z / scalar, self.w)
 
     def __floordiv__(self, scalar):
-        return vec3d(self.x // scalar, self.y // scalar, self.z // scalar, self.__w)
+        return Vec3d(self.x // scalar, self.y // scalar, self.z // scalar, self.w)
 
     def __eq__(self, o: object) -> bool:
         return type(self) == type(o) and \
             self.x == o.x and \
             self.y == o.y and \
             self.z == o.z and \
-            self.__w == o.__w
+            self.w == o.w
+
+    def __getitem__(self, index):
+        return self.__getattribute__(self.order[index])
+
+    def __setitem__(self, index, value):
+        self.__setattr__(self.order[index], value)
--- tests/test_vector.py
+++ tests/test_vector.py
@@ -1,121 +1,122 @@
+# CENG 487 Assignment1 by
+# Gokberk Akdeniz
+# StudentId:250201041
+# 10 2021
+
 import unittest
-from lib.vector import vec3d
-from math import pi
+from lib.vector import Vec3d
+from math import pi, sqrt
 
 
 class vec3dTest(unittest.TestCase):
     def setUp(self):
-        self.point = vec3d.point(1, 2, 3)
-        self.point_one = vec3d.point(1, 1, 1)
-        self.vector = vec3d.vector(1, 2, 3)
-        self.vector_one = vec3d.vector(1, 1, 1)
+        self.point = Vec3d.point(1, 2, 3)
+        self.point_one = Vec3d.point(1, 1, 1)
+        self.vector = Vec3d.vector(1, 2, 3)
+        self.vector_one = Vec3d.vector(1, 1, 1)
+        self.vector_one_right = Vec3d.vector(1, 0, 0)
+        self.vector_zero = Vec3d.vector(0, 0, 0)
+
+    def test_equality(self):
+        self.assertEqual(self.point, self.point)
+        self.assertEqual(Vec3d.vector(1, 2, 3), Vec3d.vector(1, 2, 3))
+
+    def test_clone(self):
+        self.assertEqual(self.point, self.point.clone())
 
     def test_multiplication_by_number(self):
-        expected = vec3d.point(2, 2, 2)
+        expected = Vec3d(2, 2, 2, 2)
         self.assertEqual(self.point_one * 2, expected)
         self.assertEqual(2 * self.point_one, expected)
 
-        expected = vec3d.point(2, 4, 6)
+        expected = Vec3d(2, 4, 6, 2)
         self.assertEqual(self.point * 2, expected)
         self.assertEqual(2 * self.point, expected)
 
-        expected = vec3d.point(pi, pi, pi)
+        expected = Vec3d(pi, pi, pi, pi)
         self.assertEqual(self.point_one * pi, expected)
         self.assertEqual(pi * self.point_one, expected)
 
-        expected = vec3d.point(pi, 2 * pi, 3 * pi)
+        expected = Vec3d(pi, 2 * pi, 3 * pi, pi)
         self.assertEqual(self.point * pi, expected)
         self.assertEqual(pi * self.point, expected)
 
     def test_division_by_number(self):
-        self.assertEqual(self.point_one / 2, vec3d.point(1/2, 1/2, 1/2))
-        self.assertEqual(self.point / 2, vec3d.point(1/2, 2/2, 3/2))
-        self.assertEqual(self.point_one / pi, vec3d.point(1/pi, 1/pi, 1/pi))
-        self.assertEqual(self.point / pi, vec3d.point(1/pi, 2/pi, 3/pi))
+        self.assertEqual(self.point_one / 2, Vec3d.point(1/2, 1/2, 1/2))
+        self.assertEqual(self.point / 2, Vec3d.point(1/2, 2/2, 3/2))
+        self.assertEqual(self.point_one / pi, Vec3d.point(1/pi, 1/pi, 1/pi))
+        self.assertEqual(self.point / pi, Vec3d.point(1/pi, 2/pi, 3/pi))
 
     def test_vector_addition(self):
-        self.assertEqual(self.vector + self.vector_one, vec3d.vector(2, 3, 4))
+        self.assertEqual(self.vector + self.vector_one, Vec3d.vector(2, 3, 4))
 
     def test_point_addition(self):
-        self.assertEqual(self.point + self.point_one, vec3d.point(2, 3, 4))
+        self.assertEqual(self.point + self.point_one, Vec3d(2, 3, 4, 2))
 
     def test_unary_minus(self):
-        self.assertEqual(-self.point_one, vec3d.point(-1, -1, -1))
-        self.assertEqual(-self.vector_one, vec3d.vector(-1, -1, -1))
+        self.assertEqual(-self.point_one, Vec3d.point(-1, -1, -1))
+        self.assertEqual(-self.vector_one, Vec3d.vector(-1, -1, -1))
 
     def test_unary_minus(self):
         self.assertEqual(+self.point_one, self.point_one)
         self.assertEqual(+self.vector_one, self.vector_one)
 
     def test_length(self):
-        pass
+        self.assertEqual(self.point_one.length(), 2)
+        self.assertEqual(self.vector.length(), sqrt(14))
 
     def test_dot_product(self):
-        pass
+        self.assertEqual(self.vector.dot(self.point_one), 1+2+3+0)
 
     def test_cross_product(self):
-        pass
+        self.assertEqual(self.vector.cross(self.vector), self.vector_zero)
+        self.assertEqual(self.vector.cross(self.vector_one), -self.vector_one.cross(self.vector))
 
     def test_angle(self):
-        pass
+        self.assertEqual(self.vector.angle(self.vector), 0)
+        self.assertAlmostEqual(Vec3d.vector(1, 0, 1).angle(Vec3d.vector(1, 0, 0)), pi/4)
 
     def test_project(self):
         pass
 
     def test_normalize(self):
         self.assertEqual(self.point_one.normalize(),
-                         vec3d.point(1/2, 1/2, 1/2))
-        self.assertEqual(vec3d.vector(3, 4, 0).normalize(),
-                         vec3d.vector(3/5, 4/5, 0))
+                         Vec3d.point(1/2, 1/2, 1/2))
+        self.assertEqual(Vec3d.vector(3, 4, 0).normalize(),
+                         Vec3d.vector(3/5, 4/5, 0))
 
-    def test_point_minus_vector_must_raise_type_error(self):
-        with self.assertRaises(TypeError):
-            self.point - self.vector
-
-    def test_vector_minus_point_must_raise_type_error(self):
-        with self.assertRaises(TypeError):
-            self.vector - self.point
-
-    def test_point_plus_vector_must_raise_type_error(self):
-        with self.assertRaises(TypeError):
-            self.point + self.vector
-
-    def test_vector_plus_point_must_raise_type_error(self):
-        with self.assertRaises(TypeError):
-            self.vector + self.point
-
-    def test_point_multiply_by_point_must_raise_type_error(self):
+    def test_vec3d_multiply_by_nonnumber_must_raise_type_error(self):
         with self.assertRaises(TypeError):
             self.point_one * self.point_one
-
-    def test_point_multiply_by_vector_must_raise_type_error(self):
-        with self.assertRaises(TypeError):
             self.point_one * self.vector_one
-
-    def test_vector_multiply_by_point_must_raise_type_error(self):
-        with self.assertRaises(TypeError):
             self.vector_one * self.point_one
-
-    def test_vector_multiply_by_vector_must_raise_type_error(self):
-        with self.assertRaises(TypeError):
             self.vector_one * self.vector_one
+            self.vector_one * object()
 
-    def test_point_divided_by_point_must_raise_type_error(self):
+    def test_vec3d_divided_by_nonnumber_must_raise_type_error(self):
         with self.assertRaises(TypeError):
             self.point / self.point_one
-
-    def test_vector_divided_by_vector_must_raise_type_error(self):
-        with self.assertRaises(TypeError):
             self.vector / self.vector_one
+            self.vector / object()
 
-    def test_number_divided_by_point_must_raise_type_error(self):
+    def test_number_divided_by_vec3d_must_raise_type_error(self):
         with self.assertRaises(TypeError):
             1 / self.point
-
-    def test_number_divided_by_vector_must_raise_type_error(self):
-        with self.assertRaises(TypeError):
             1 / self.vector
 
+    def test_getitem(self):
+        self.assertEqual(self.vector[0], 1)
+        self.assertEqual(self.vector[1], 2)
+        self.assertEqual(self.vector[2], 3)
+        self.assertEqual(self.vector[3], 0)
+
+    def test_setitem(self):
+        vec = self.vector_zero.clone()
+        vec[0] = 1
+        vec[1] = 2
+        vec[2] = 3
+        vec[3] = 1
+        self.assertEqual(vec, self.point)
 
 if __name__ == '__main__':
     unittest.main()
