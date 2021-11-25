425bc94111c57bcf8cff46f2d1d1f5e95bf1db18
Gökberk AKDENİZ <gokberkakdeniz@hotmail.com>
2021-11-25 13:50:16 +0300

    drop glTranslate usage

--- lib/ui/camera.py
+++ lib/ui/camera.py
@@ -10,29 +10,25 @@ from ..math import Mat3d
 
 class Camera:
     def __init__(self) -> None:
-        self.matrix = Mat3d.identity()
+        self.matrix = Mat3d.translation_matrix(0, 0, -6)
 
     def zoom_in(self, factor=1.25):
-        self.matrix = Mat3d.scaling_matrix(
+        self.matrix @= Mat3d.scaling_matrix(
             factor, factor, factor
-        ) @ self.matrix
+        )
 
     def zoom_out(self, factor=0.8):
-        self.matrix = Mat3d.scaling_matrix(
+        self.matrix @= Mat3d.scaling_matrix(
             factor, factor, factor
-        ) @ self.matrix
+        )
 
     def rotate(self, x: float, y: float, z: float):
-        self.matrix = Mat3d.rotation_z_matrix(z) \
+        self.matrix @= Mat3d.rotation_z_matrix(z) \
             @ Mat3d.rotation_y_matrix(y) \
-            @  Mat3d.rotation_x_matrix(x) \
-            @ self.matrix
+            @ Mat3d.rotation_x_matrix(x)
 
     def reset(self):
-        self.matrix = Mat3d.identity()
+        self.matrix = Mat3d.translation_matrix(0, 0, -6)
 
     def look(self):
-        glLoadIdentity()
-        # i could not convert this to translation matrix... :(
-        glTranslatef(0, 0, -6)
-        glMultMatrixf(self.matrix.to_array())
+        glLoadMatrixf(self.matrix.to_array())
